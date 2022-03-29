7adbf2f543d616856f1afb51bb4c1c0b90a7b4d3
GET Academy <terje@getacademy.no>
2022-03-29 10:32:33 +0200

    Initial commit

---
+++ MyListOO/MyList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyListOO
+{
+    internal class MyList
+    {
+        string[] _string;
+        int _count;
+
+        public MyList()
+        {
+            _string = new string[3];
+            _count = 0;
+        }
+
+        public void Add(string text)
+        {
+            if (_count >= _string.Length) Grow();
+            _string[_count] = text;
+            _count++;
+        }
+
+        private void Grow()
+        {
+            Console.WriteLine($"Dobler kapasitet fra {_string.Length} til {_string.Length * 2}");
+            var newStrings = new string[_string.Length * 2];
+            Array.Copy(_string, newStrings, _string.Length);
+            _string = newStrings;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < _string.Length; i++)
+            {
+                Console.WriteLine($"index {i} => {_string[i]} ");
+            }
+        }
+    }
+}
---
+++ MyListOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyListOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Pause til 10:38
+            */
+            var myList = new MyList();
+
+            while (true)
+            {
+                Console.Write("Skriv et ord: ");
+                var word = Console.ReadLine();
+                myList.Add(word);
+                myList.Show();
+            }
+        }
+    }
+}
