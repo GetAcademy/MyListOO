d8d9c0b562cf6f93b52e0992d00e6d576ca97a31
GET Academy <terje@getacademy.no>
2022-03-29 10:46:39 +0200

    x

---
+++ MyListOO/MyGenericList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyListOO
+{
+    internal class MyGenericList<T>
+    {
+        T[] _values;
+        int _count;
+
+        public MyGenericList()
+        {
+            _values = new T[3];
+            _count = 0;
+        }
+
+        public void Add(T value)
+        {
+            if (_count >= _values.Length) Grow();
+            _values[_count] = value;
+            _count++;
+        }
+
+        private void Grow()
+        {
+            Console.WriteLine($"Dobler kapasitet fra {_values.Length} til {_values.Length * 2}");
+            var newValues = new T[_values.Length * 2];
+            Array.Copy(_values, newValues, _values.Length);
+            _values = newValues;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < _values.Length; i++)
+            {
+                Console.WriteLine($"index {i} => {_values[i]} ");
+            }
+        }
+    }
+
+}
+
--- MyListOO/Program.cs
+++ MyListOO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyListOO
 {
@@ -6,18 +7,54 @@ namespace MyListOO
     {
         static void Main(string[] args)
         {
-            /*
-            Pause til 10:38
-            */
-            var myList = new MyList();
+            var myList1 = new List<int>();
+            var myList2 = new List<float>();
 
             while (true)
             {
-                Console.Write("Skriv et ord: ");
-                var word = Console.ReadLine();
-                myList.Add(word);
-                myList.Show();
+                Console.Write("Skriv et heltall: ");
+                var nStr = Console.ReadLine();
+                var n = Convert.ToInt32(nStr);
+                myList1.Add(n);
+
+                Console.Write("Skriv et desimaltall: ");
+                var fStr = Console.ReadLine();
+                var f = Convert.ToSingle(fStr);
+                myList2.Add(f);
+
+                //myList1.Show();
+                //myList2.Show();
             }
+
+            //var myList1 = new MyGenericList<int>();
+            //var myList2 = new MyGenericList<float>();
+
+            //while (true)
+            //{
+            //    Console.Write("Skriv et heltall: ");
+            //    var nStr = Console.ReadLine();
+            //    var n = Convert.ToInt32(nStr);
+            //    myList1.Add(n);
+
+            //    Console.Write("Skriv et desimaltall: ");
+            //    var fStr = Console.ReadLine();
+            //    var f = Convert.ToSingle(fStr);
+            //    myList2.Add(f);
+
+            //    myList1.Show();
+            //    myList2.Show();
+            //}
+
+
+            //var myList = new MyList();
+
+            //while (true)
+            //{
+            //    Console.Write("Skriv et ord: ");
+            //    var word = Console.ReadLine();
+            //    myList.Add(word);
+            //    myList.Show();
+            //}
         }
     }
 }
